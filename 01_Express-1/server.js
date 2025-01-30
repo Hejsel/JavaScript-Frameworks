9cad25b6743cbf059d83ae5500851f81beda2863
Hejsel <benjaminhejsel@gmail.com>
2025-01-30 21:17:00 +0100

    masser af kode.

---
+++ 01_Express-1/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const IP = 'localhost';
+const path = require('path');
+
+const nav = `<nav>
+	<a href="/">Hjem</a>
+	<a href="/oversigt">Oversigt</a>
+	<a href="/login">Login</a>
+</nav>`;
+
+app.use('/images', express.static('01_Express-1/images/offentlige'));
+
+/*
+app.get('/:filename(login|oversigt)', (req, res) => {
+	res.sendFile(path.join(__dirname, req.params.filename + '.html'));
+}); //dette er reelt en hændelseslytter, req er objektet for anmodningen, res er objektet for responsen
+*/
+
+/*
+app.get('/login/:filename', (req, res) => {
+	//res.setHeader('Content-Type', 'text/html');
+	res.sendFile(path.join(__dirname, '/login.html'));
+});
+
+app.get('/oversigt/:filename', (req, res) => {
+	//res.setHeader('Content-Type', 'text/html');
+	res.sendFile(path.join(__dirname, '/oversigt.html'));
+});
+*/
+app.listen(port, IP, () => {
+	console.log(`Serveren kører på http://${IP}:${port}`); //det er selvfølgelig ikke obligatorisk at have dette console-statement
+});
