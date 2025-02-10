481a6c65c620e9b3b6c750618a0e09688ca0f949
Hejsel <benjaminhejsel@gmail.com>
2025-02-10 21:51:22 +0100

    kode

--- 02_Express-2/server/opgave1.js
+++ 02_Express-2/server/opgave1.js
@@ -5,12 +5,12 @@ const IP = 'localhost';
 const path = require('path');
 const filmappe = path.join(__dirname, '../client/');
 
-// ➤ Gør "/" til at returnere opgave1.html automatisk
+// ➤ Sætter en statisk routing, hvor endpointet "/" returnere variablen filmappe som indeholder stien til indholdet i mappen client/ som så er sat til filen opgave1.html
 app.get('/', (req, res) => {
 	res.sendFile(filmappe + 'opgave1.html');
 });
 
-// ➤ Loader filer som f.eks. "opgave1.html"
+// ➤ Loader filer via dynamisk routing som f.eks. "opgave1", "opgave2" osv. ved at kople en del af URL'en som en variabel/parameter ved navn fil ved at definere et parameter i URL'en med ":", sammen med req.params.fil
 app.get('/:fil', (req, res, next) => {
 	let fil = req.params.fil;
 	if (fil.startsWith('opgave')) {
@@ -19,7 +19,7 @@ app.get('/:fil', (req, res, next) => {
 	} else next();
 });
 
-// ➤ Multiplikations-API
+// ➤ Multiplikations-API som loades via en specifik routing med endpointet /mult
 app.get('/mult', (req, res) => {
 	let x = parseFloat(req.query.x);
 	let y = parseFloat(req.query.y);
